c96449335a34671d1ba3f94fd0aa84792089a101
nanhtu93 <nanhtu93@gmail.com>
2021-10-27 03:36:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bot = new TeleBot('1964935638:AAElbpCVYgMPwqtH39VolBmQ5Q6iIGQ89tM');
 
 bot.on('/start', (msg) => {
     return bot.sendMessage(msg.from.id, `Xin Chào ${ msg.from.first_name } ${ msg.from.last_name }
-Cảm ơn bạn đã trở thành thành viên của <b>Magic's Forex</b>.
+Cảm ơn bạn đã trở thành thành viên của Magic's Forex.
     
 ID MGFX Của Bạn: ${ msg.from.id }`);
 
