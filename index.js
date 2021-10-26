ece91c217085f0ee0e376633d9017953bc9f1208
MagicsTeam <nanhtu93@gmail.com>
2021-10-27 03:20:11 +0700

    Exemple telegram bot using nodejs

---
+++ index.js
@@ -0,0 +1,13 @@
+const TeleBot = require('telebot');
+const bot = new TeleBot('1964935638:AAElbpCVYgMPwqtH39VolBmQ5Q6iIGQ89tM');
+
+bot.on('/start', (msg) => {
+    return bot.sendMessage(msg.from.id, `Xin Chào ${ msg.from.first_name } ${ msg.from.last_name }
+Cảm ơn bạn đã trở thành thành viên của Magic's Forex.
+    
+ID MGFX Của Bạn: ${ msg.from.id }`);
+
+});
+
+
+bot.start();
